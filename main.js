0f01909515da657cb088e675bd8629f259b50132
sohaila <sohailahazem5@gmail.com>
2023-03-06 03:52:14 +0200

    upload project

---
+++ main.js
@@ -0,0 +1,26 @@
+const Img = document.querySelectorAll('.text img')
+const Span = document.querySelectorAll('.text span')
+const Paragraph = document.querySelectorAll('p')
+
+// console.log(Paragraph)
+
+Img[0].onclick = function(){
+    Span[0].style.fontWeight='700'
+    Paragraph[0].style.display = 'flex'
+}
+Img[1].onclick = function(){
+    Span[1].style.fontWeight='700'
+    Paragraph[1].style.display = 'flex'
+}
+Img[2].onclick = function(){
+    Span[2].style.fontWeight='700'
+    Paragraph[2].style.display = 'flex'
+}
+Img[3].onclick = function(){
+    Span[3].style.fontWeight='700'
+    Paragraph[3].style.display = 'flex'
+}
+Img[4].onclick = function(){
+    Span[4].style.fontWeight='700'
+    Paragraph[4].style.display = 'flex'
+}
